91d43a1fac68baf45daa6a8e1f836334a07a9930
Chris Conley <chrisc93@vt.edu>
2015-01-12 15:43:11 -0500

    Cleaning up fahrenheit output

--- temp.py
+++ temp.py
@@ -6,5 +6,7 @@ raw_temp = file.readline()
 cel_string = raw_temp[0] + raw_temp[1] + '.' + raw_temp[2]
 cel_float = float(cel_string)
 far_float = (cel_float*1.8000) + 32.00
+far_raw_s = str(far_float)
+far_arr = far_raw_s.split('.')
 print(cel_string + "'C")
-print(str(far_float) + "'F")
+print(far_arr[0] + "." + far_arr[1][0] + "'F")
